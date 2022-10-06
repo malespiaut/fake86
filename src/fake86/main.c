4651bfa9a239b84b7012ad33b0fd585d5d66af77
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2022-10-06 22:25:50 +0200

    Renaming rombasic.bin to ibm-basic-1.10.rom. This file is the combination of the four ROMs (5000019 (U29), 5000021 (U30), 5000022 (U31), 5000023 (U32)) from the rev 3 (10/27/82) IBM PC 5150, that forms the ROM Basic 1.1.

--- src/fake86/main.c
+++ src/fake86/main.c
@@ -308,7 +308,7 @@ main(int argc, char* argv[])
 #endif
   if (biossize <= 8192)
     {
-      loadrom(0xF6000UL, PATH_DATAFILES "rombasic.bin", 0);
+      loadrom(0xF6000UL, PATH_DATAFILES "ibm-basic-1.10.rom", 0);
       if (!loadrom(0xC0000UL, PATH_DATAFILES "videorom.bin", 1))
         {
           return (-1);
